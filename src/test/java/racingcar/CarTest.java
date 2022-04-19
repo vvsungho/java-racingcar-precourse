009402af42fb0feec592d8a1dabe11ce5ea0c8da
shyoon <shyoon@12cm.co.kr>
2022-04-19 23:12:51 +0900

    test: Car Name 테스트 케이스 코드 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,28 +2,40 @@ package racingcar;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.CarNames;
+import racingcar.domain.CarNumber;
+import racingcar.domain.CarStatus;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CarTest {
     private Car car;
+    private CarNumber carNumber;
+    private CarNames carNames;
 
     @BeforeEach
     public void setUp() {
         car = new Car("A", 1);
+        carNumber = new CarNumber(5);
+        carNames = new CarNames("yoond,eundo,minbo");
     }
 
     @Test
-    public void validateInputNumberTest() {
-        boolean result = CarNumber.validateNumber(0);
-        assertTrue(result);
+    public void validateInputNumberThrowTest() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> carNumber.validateNumber(10)
+        );
     }
 
     @Test
-    public void validateInputNameTest() {
-        boolean result = CarName.validateName();
-        assertTrue(result);
+    public void validateInputNameThrowTest() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> carNames.validateNames(carNames.getNames())
+        );
     }
 
     @Test
