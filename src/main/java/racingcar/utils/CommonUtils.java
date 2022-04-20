548614fb5fcbd21cad441210284b725b48ec73d9
shyoon <shyoon@12cm.co.kr>
2022-04-20 19:34:11 +0900

    feat: 공통 Util 클래스 생성

--- src/main/java/racingcar/utils/CommonUtils.java
+++ src/main/java/racingcar/utils/CommonUtils.java
@@ -1,7 +1,22 @@
 package racingcar.utils;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class CommonUtils {
+    public static final String COMMA = ",";
+    public static final int MIN_VALUE = 0;
+    public static final int MAX_VALUE = 9;
+
     public static boolean isNullOrEmptyString(String str) {
         return str == null || str == "";
+    }
+
+    public static String inputText() {
+        return Console.readLine();
+    }
+
+    public static int getRandomNumber(int startNumber, int endNumber) {
+        return Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
     }
 }
