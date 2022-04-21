74bc77c2d7cea98ad711540d06228d791fa71a4f
vvsungho <vvsungho@gmail.com>
2022-04-21 23:47:04 +0900

    test: 레이싱게임 우승자 선정 테스트 케이스 작성

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,9 +1,16 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RacingGameTest {
+    public static final String COLON = ":";
     private RacingGame racingGame;
 
     @BeforeEach
@@ -11,14 +18,22 @@ public class RacingGameTest {
         racingGame = new RacingGame("yoon,lee,kim");
     }
 
-    @Test
-    void testMethod() {
+    @ParameterizedTest
+    @DisplayName("우승자 선정 테스트")
+    @CsvSource(value = {"9,1,2:yoon", "5,5,1:yoon,lee", "5,5,5:yoon,lee,kim"}, delimiter = ':')
+    void validateWinnerNamesTest(String input, String expected) {
         // given
-        for (Car car : racingGame.getResult().getCars()) {
-            car.move(2);
-        }
-        // when
+        String[] splitCarNumber = input.split(COLON)[0].split(",");
+        List<Car> resultCar = racingGame.getResult().getCars();
 
+        // when
+        for (int i = 0; i < splitCarNumber.length; i++) {
+            Car car = resultCar.get(i);
+            car.move(Integer.parseInt(splitCarNumber[i]));
+        }
+        String result = Cars.getWinnerNames(racingGame.getResult().getWinnerCars());
+        
         // then
+        assertEquals(expected, result);
     }
 }
