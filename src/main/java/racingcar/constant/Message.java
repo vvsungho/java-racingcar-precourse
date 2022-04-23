fbdf98186bc4514d4a22dd51fa3dd6cd99869ad0
vvsungho <vvsungho@gmail.com>
2022-04-23 21:19:31 +0900

    feat: 게임 실행 controller 구현

--- src/main/java/racingcar/constant/Message.java
+++ src/main/java/racingcar/constant/Message.java
@@ -6,9 +6,13 @@ import static racingcar.constant.CarConstant.MIN_VALUE;
 public class Message {
     public static final String COLON = ":";
     public static final String COMMA = ",";
+    public static final String DASH = "-";
+    public static final String SPACING = " ";
     public static final String PREFIX_ERROR = "[ERROR] ";
     public static final String ERROR_INPUT_NULL_OR_EMPTY = "값을 입력해주세요.";
     public static final String ERROR_NUMBER_RANGE = PREFIX_ERROR + MIN_VALUE + "~" + MAX_VALUE + "사이의 숫자로 입력해주세요.";
     public static final String ERROR_NAME_LENGTH = PREFIX_ERROR + "이름은 5글자 이하로 입력해주세요.";
-    public static final String ERROR_INPUT_TRY_NUMBER = "숫자만 입력 가능합니다.";
+    public static final String ERROR_INPUT_TRY_NUMBER = PREFIX_ERROR + "숫자만 입력 가능합니다.";
+    public static final String RACING_RESULT = "실행 결과";
+    public static final String WINNER = "최종 우승자는 #{winnerName} 입니다.";
 }
--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,14 +1,48 @@
 package racingcar.controller;
 
-import racingcar.domain.CarName;
-import racingcar.utils.CommonUtils;
+import racingcar.domain.CarNames;
+import racingcar.domain.CarNumber;
+import racingcar.domain.RacingGame;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class GameController {
-    public void play() {
-        InputView.inputRacingCarNames();
-        CarName carName = new CarName(CommonUtils.inputText());
+    CarNames carNames;
+    CarNumber carNumber;
 
-        InputView.inputTryCount();
+    public void play() {
+        boolean isValidInputNames = false;
+
+        while (!isValidInputNames) {
+            try {
+                inputAndCreateCarNames();
+                isValidInputNames = true;
+            } catch (IllegalArgumentException illegalArgumentException) {
+                OutputView.printMessage(illegalArgumentException.getMessage());
+            }
+        }
+
+        boolean isValidInputNumber = false;
+        while (!isValidInputNumber) {
+            try {
+                inputAndCreateCarNumber();
+                isValidInputNumber = true;
+            } catch (IllegalArgumentException illegalArgumentException) {
+                OutputView.printMessage(illegalArgumentException.getMessage());
+            }
+        }
+
+        RacingGame racingGame = new RacingGame(carNames.getNames());
+        racingGame.play(carNumber.getNumber());
+
+        OutputView.printRacingResult(racingGame);
+    }
+
+    private void inputAndCreateCarNames() {
+        carNames = new CarNames(InputView.printInputRacingCarNames());
+    }
+
+    private void inputAndCreateCarNumber() {
+        carNumber = new CarNumber(InputView.printInputTryCount());
     }
 }
